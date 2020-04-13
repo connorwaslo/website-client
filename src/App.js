2db4c4c5d0ba7840ccabc362e87a205f021c5349
johnazre <jaboyak@gmail.com>
2020-04-13 16:56:52 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './App.css'
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  )
+}
+
+export default App
